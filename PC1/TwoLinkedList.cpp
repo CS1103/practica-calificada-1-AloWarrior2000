ea4594c6af42b1325d36e2c69b4092655a161e3b
Christian Ledgard <christianledgard@gmail.com>
2019-04-12 13:11:02 -0500

    Primer avance PC1

--- PC1/TwoLinkedList.cpp
+++ PC1/TwoLinkedList.cpp
@@ -3,3 +3,127 @@
 //
 
 #include "TwoLinkedList.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+using namespace UTEC;
+
+int TwoLinkedList::size(int list) {
+    if (list == 1) {
+        int count = 0;
+        Node *temp = head1;
+        while (temp != nullptr) {
+            count++;
+            temp = temp->next;
+        }
+        return count;
+    }
+    else if(list == 2){
+        int count = 0;
+        Node *temp = head2;
+        while (temp != nullptr) {
+            count++;
+            temp = temp->next;
+        }
+        return count;
+    }
+    else
+        return 0;
+}
+
+void TwoLinkedList::push_back1(int Nvalue) {
+    Node* temp = new Node(Nvalue);
+    if (size(1) == 0){
+        tail1 = temp;
+        head1 = temp;
+    }
+    else{
+        tail1->next = temp;
+        tail1 = temp;
+    }
+}
+
+void TwoLinkedList::push_back2(int Nvalue) {
+    Node* temp = new Node(Nvalue);
+    if (size(2) == 0){
+        tail2 = temp;
+        head2 = temp;
+    }
+    else{
+        tail2->next = temp;
+        tail2 = temp;
+    }
+}
+
+bool TwoLinkedList::is_merged() {
+    Node *aux1 = head1;
+    Node *aux2 = head2;
+    for (int i = 0; i < size(2); i++) {
+        for (int j = 0; j < size(1); ++j) {
+            if (aux1 == aux2) {
+                return true;
+            }
+            aux1 = aux1->next;
+        }
+        aux2 = aux2->next;
+    }
+    return false;
+}
+string TwoLinkedList::merge(int Nvalue) {
+    if (!is_merged()) {
+        Node *temp = new Node(Nvalue);
+        tail1->next = temp;
+        tail2->next = temp;
+        tail1 = temp;
+        tail2 = temp;
+        tail = temp;
+        return "Operación exitosa";
+
+    }
+    else{return "Operación duplicada";}
+}
+
+string TwoLinkedList::getlist(int list) {
+    string text = "";
+    Node *aux = nullptr;
+    if (list == 1) {
+        aux = head1;
+        for (int i = 0; i < size(1); i++) {
+        text = text + to_string(aux->value) + " ";
+        aux = aux->next;
+    }
+    }else if(list == 2){
+        aux = head2;
+        for (int j = 0; j < size(2);j++) {
+            text = text + to_string(aux->value) + " ";
+            aux = aux->next;
+        }
+
+
+    }else {
+        if(is_merged()){
+            Node *aux1 = head1;
+            Node *aux2 = head2;
+            for (int i = 0; i < size(2); i++) {
+                for (int j = 0; j < size(1); ++j) {
+                    if (aux1 == aux2) {
+                        text = text + to_string(aux1->value) + " ";
+                    }
+                    aux1 = aux1->next;
+                }
+                aux2 = aux2->next;
+            }
+        }
+    }
+    return text;
+}
+
+Node *search() {}
+
+void save(string root) {
+
+}
+
+void load() {}
--- PC1/TwoLinkedList.h
+++ PC1/TwoLinkedList.h
@@ -5,10 +5,53 @@
 #ifndef PC1_TWOLINKEDLIST_H
 #define PC1_TWOLINKEDLIST_H
 
+#include <string>
+#include <iostream>
 
-class TwoLinkedList {
+struct Node{
+    int value;
+    Node* next;
+
+    Node(int val) : value(val), next(nullptr){}
+
+
+
 
 };
 
+
+namespace UTEC {
+
+    class TwoLinkedList {
+        Node *head1;
+        Node *head2;
+        Node *tail1;
+        Node *tail2;
+        Node *tail;
+
+    public:
+        TwoLinkedList() {
+            head1 = nullptr;
+            head2 = nullptr;
+            tail1 = nullptr;
+            tail2 = nullptr;
+            tail = nullptr;
+        }
+
+        void push_back1(int Nvalue);
+        void push_back2(int Nvalue);
+        std::string merge(int Nvalue);
+        bool is_merged();
+        std::string getlist(int list);
+        Node *search();
+        int size(int list);
+        void save(std::string root);
+        void load();
+
+
+    };
+
+
+}
 
 #endif //PC1_TWOLINKEDLIST_H
